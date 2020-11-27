f8e77580a0b7293bd411b3396a24f634a7ff87e4
Nick Oliveira <Nicholas@oliveira.me>
2020-11-27 11:30:40 -0800

    Bug fixes on budget search

--- functions/index.js
+++ functions/index.js
@@ -50,13 +50,13 @@ app.get('/api/offerings/bids/:currency', (req, res) => {
 
 //relates to user searching how much they can buy of each crypto from different exchanges
 //with respect to their current budget
-app.get('/api/offerings/asks/budget/:currency/:value', (req,res) => {
+app.get('/api/offerings/asks/budget/:currency/:amount', (req,res) => {
     //strating path for the database
-    let databasePath = "TRADES/asks/" + req.params.currency;
+    const databasePath = "TRADES/asks/" + req.params.currency;
     //empty output json object
     let jsonOutput = `{"offerings": []}`;
     //allows us to add smaller json objects to offerings array in output
-    var output = JSON.parse(jsonOutput);
+    let output = JSON.parse(jsonOutput);
     //order children by price so we can compare with budget constraint
     db.ref(databasePath).on('value', (snapshot) => {
             let result = snapshot.val();
@@ -66,22 +66,29 @@ app.get('/api/offerings/asks/budget/:currency/:value', (req,res) => {
                 
                 //for each crypto offered in the exchange
                 for(crypto in result[exchange]) {
-                    let budget = req.params.value;     
+                    let budget = req.params.amount;  
+                    //console.log(budget); 
                     
-                    let offerings = result[exchange][crypto];                    
+                    const offerings = result[exchange][crypto];                               
                     let i = 0;
                     let amount = 0;
 
                     while(budget > 0) {
-                        let offeringCost = parseFloat(offerings[i].price);
-                        let offeringSize = parseFloat(offerings[i++].size);
+                        const offeringCost = parseFloat(offerings[i].price);
+                        const offeringSize = parseFloat(offerings[i++].size);
+
+                        //console.log(offeringCost + " X " + offeringSize);
+                        
+                        //if we need to take a fraction of the offering
+                        console.log("Cost for " + exchange + " at " + crypto + ":" + (offeringCost*offeringSize));
                         if(budget < (offeringCost*offeringSize)) {
                             let ratio = budget / offeringCost;
-                            budget -= offeringCost * offeringSize * ratio;
-                            amount += offeringSize * ratio;
+                            //console.log("ratio: " + ratio)
+                            budget -= (offeringCost * offeringSize) * ratio;
+                            amount += ratio;
 
                         } else {
-                            budget -= offeringCost * offeringSize;
+                            budget -= (offeringCost * offeringSize);
                             amount += offeringSize;
                         }
                     }
@@ -102,51 +109,63 @@ app.get('/api/offerings/asks/budget/:currency/:value', (req,res) => {
     })
 })
 
-app.get('/api/offerings/bids/budget/:cryptoCurrency/:amount', (req, res) => {
-    //strating path for the database
-    let databasePath = "TRADES/bids/";
-    //empty output json object
-    let jsonOutput = `{"offerings": []}`;
-    //allows us to add smaller json objects to offerings array in output
-    var output = JSON.parse(jsonOutput);
-    //order children by price so we can compare with budget constraint
-    db.ref(databasePath).on('value', (snapshot) => {
-            let result = snapshot.val();
-            for(currency in result) {
-                for(exchange in result[currency]) {
-                    let offerings = result[currency][exchange][req.params.cryptoCurrency];
-                    let amount = req.params.amount;
-                    let price = 0;
-                    let i = 0;
+app.get('/api/offerings/bids/budget/:currency/:amount', (req, res) => {
+  //strating path for the database
+  const databasePath = "TRADES/bids/" + req.params.currency;
+  //empty output json object
+  let jsonOutput = `{"offerings": []}`;
+  //allows us to add smaller json objects to offerings array in output
+  let output = JSON.parse(jsonOutput);
+  //order children by price so we can compare with budget constraint
+  db.ref(databasePath).on('value', (snapshot) => {
+          let result = snapshot.val();
+          
+          //for each exchange offered
+          for(exchange in result) {                       
+              
+              //for each crypto offered in the exchange
+              for(crypto in result[exchange]) {
+                  let budget = req.params.amount;  
+                  console.log(budget); 
+                  
+                  const offerings = result[exchange][crypto];                               
+                  let i = 0;
+                  let amount = 0;
 
-                    while(amount > 0) {
-                        let offeringCost = parseFloat(offerings[i].price);
-                        let offeringSize = parseFloat(offerings[i++].size);
+                  while(budget > 0) {
+                      const offeringCost = parseFloat(offerings[i].price);
+                      const offeringSize = parseFloat(offerings[i++].size);
 
-                        if(amount < offeringSize) {
-                            let ratio = amount / offeringSize;
-                            amount -= offeringSize;
-                            price += (offeringCost * offeringSize) * ratio;
-                        } else {
-                            amount -= offeringSize;
-                            price += offeringCost * offeringSize;
-                        }
-                    }
+                      //console.log(offeringCost + " X " + offeringSize);
+                      
+                      //if we need to take a fraction of the offering
+                      //console.log("Cost for " + exchange + " at " + crypto + ":" + (offeringCost*offeringSize));
+                      if(budget < (offeringCost*offeringSize)) {
+                          let ratio = budget / offeringCost;
+                          //console.log("ratio: " + ratio)
+                          budget -= (offeringCost * offeringSize) * ratio;
+                          amount += ratio;
 
-                    output['offerings'].push(
-                        { 
-                            "Exchange": exchange, 
-                            "CryptoCurrency" : req.params.cryptoCurrency,
-                            "Amount": req.params.amount,
-                            "Currency": currency,
-                            "Price" : price
-                        });
-                }
-            }
-            
-           res.json(output);
+                      } else {
+                          budget -= (offeringCost * offeringSize);
+                          amount += offeringSize;
+                      }
+                  }
 
-    })
+                  output['offerings'].push(
+                      { 
+                          "Exchange": exchange, 
+                          "CryptoCurrency" : crypto,
+                          "Amount": amount,
+                          "Currency": req.params.currency,
+                          "Price" : req.params.value
+                      });
+              }                
+             
+          }
+         res.json(output);
+
+  })
 })
 
 
